ffb2855e8dd0ef650c92c304cda72af2fe453fdf
Moravec-Jan <hmoravecb@gmail.com>
2018-04-12 14:01:36 +0200

    hibernate orm

---
+++ mysql/create.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE country
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(60) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE town
+(
+	id INT NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    country_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+ALTER TABLE town ADD FOREIGN KEY (country_id) REFERENCES country (id);
+CREATE INDEX fk_town_country_id ON town (country_id);
---
+++ mysql/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO country (name)
+VALUES ("Czech Republic"),
+	   ("USA"),
+       ("Japan");
+       
+INSERT INTO town (id,name,country_id)
+VALUES (100,"Prague",1),
+	   (101,"Brno",1),
+	   (102,"Liberec",1),
+	   (200,"New York",2);
